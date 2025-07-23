2ba6014a14062e48ca9ee8ffb41d0aa6ff93d8f2
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-07-23 15:16:25 +0400

    Lesson 2

--- gendiff.js
+++ gendiff.js
@@ -6,5 +6,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --vers', 'output the current version')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .argument('<string>');
 program.parse(process.argv);
