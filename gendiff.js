ed6a48e297976dfb6f09787a453e1d22a4cfa410
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-07-23 15:54:24 +0400

    Lesson 4

--- gendiff.js
+++ gendiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import { parseFile } from './parser.js';
+import _ from 'lodash';
 
 const program = new Command();
 
@@ -14,10 +15,20 @@ program
     try {
       const data1 = parseFile(filepath1);
       const data2 = parseFile(filepath2);
-      const differences = JSON.stringify(data1) === JSON.stringify(data2)
-        ? 'Файлы идентичны'
-        : 'Файлы различаются';
-      console.log(differences);
+      const allKeys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+      const differences = allKeys.map((key) => {
+        if (!_.has(data1, key)) {
+          return `+ ${key}: ${data2[key]}`;
+        }
+        if (!_.has(data2, key)) {
+          return `- ${key}: ${data1[key]}`;
+        }
+        if (!_.isEqual(data1[key], data2[key])) {
+          return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+        }
+        return `  ${key}: ${data1[key]}`;
+      });
+      console.log(differences.join('\n'));
     } catch (error) {
       console.error('Ошибка при чтении или обработке файлов:', error);
     }
