af7e8eeef45ca9cde4d40fe28465462db28cb237
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-08-15 14:44:15 +0400

    Step 2

--- gendiff.js
+++ gendiff.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
-
-import { Command } from 'commander';
+import { Command } from 'commander'
 
 const program = new Command();
 
 program
-    .name('gendiff')
-    .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.')
+  .name('gendiff')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1')
+    .argument('<filepath2')
+    .action((a, b, options) => {
+        console.log('a', a);
+        console.log('b', b);
+        console.log('options', options);
+    });
 
-program.parse(process.argv);
+program.parse()
