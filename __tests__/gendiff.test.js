512e61f83936ed9214bc46e9ad30d173765c4944
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-08-17 10:09:37 +0400

    setting up the environment for sonarcube

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,8 +6,8 @@ const getFixturesPath = (filename) => path.resolve(process.cwd(), filename)
 const result = fs.readFileSync(getFixturesPath('result.txt'), 'utf-8')
  
 test('gendiff', () => {
-    expect(gendiff(
-        getFixturesPath('file1.json'),
+    expect(
+        gendiff(getFixturesPath('file1.json'), 
         getFixturesPath('file2.json')
     )).toBe(result);
 })
--- src/index.js
+++ src/index.js
@@ -9,25 +9,24 @@ export default (filepath1, filepath2) => {
     const parsedData1 = parser(content1);
     const parsedData2 = parser(content2);
 
-    const gendiff = (obj1, obj2) => {
-        const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort();
+const gendiff = (obj1, obj2) => {
+    const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort();
 
-        const diff = keys.map((key) => {
+    const diff = keys.map((key) => {
         if (!Object.hasOwn(obj2, key)) {
-            return `    - ${key}: ${obj1[key]}`
-            }
+            return `    - ${key}: ${obj1[key]}`;
+        }
         if (!Object.hasOwn(obj1, key)) {
-            return `    + ${key}: ${obj2[key]}`
-            }
+            return `    + ${key}: ${obj2[key]}`;
+        }
         if (obj1[key] !== obj2[key]) {
-            return `    - ${key}: ${obj1[key]}\n    + ${key}: ${obj2[key]}`
-            }
-        return `    ${key}: ${obj1[key]}`
-       })
+            return `    - ${key}: ${obj1[key]}\n    + ${key}: ${obj2[key]}`;
+        }
+        return `    ${key}: ${obj1[key]}`;
+    });
 
-       return `{\n${diff.join('\n')}\n}`
-
-    }
+    return `{\n${diff.join('\n')}\n}`;
+}
 
     return gendiff(parsedData1, parsedData2)
 }
