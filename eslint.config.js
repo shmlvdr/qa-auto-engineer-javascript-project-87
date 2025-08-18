3ec87e65672c6fa06b15531af9130c4e85f57251
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-08-18 16:45:03 +0400

    fixed eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,23 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: ["coverage/lcov-report/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
 ]);
